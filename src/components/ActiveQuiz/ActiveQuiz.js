bf6bb93ca5b733e8d4e9f87928cbd577eff9f5ff
Rodion Roslovets <Rodion18570@gmail.com>
2019-12-14 14:09:05 +0300

    Created AnswerList and AnswerItem

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import './ActiveQuiz.css';
+import AnswersList from "./AnswersList/AnswersList";
 
-const ActiveQuiz = (props) => (
-    <div className='ActiveQuiz'>
-        <p className="Question">
-            <span>
-                <strong>2.</strong>
-                &nbsp;Как дела?
-            </span>
+const ActiveQuiz = (props) => {
+    
+    return(
+        <div className='ActiveQuiz'>
+            <p className="Question">
+                <span>
+                    <strong>2.</strong>
+                    { props.question }
+                </span>
 
-            <small>4 из 12</small>
-        </p>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
-    </div>
-);
+                <small>4 из 12</small>
+            </p>
+            <AnswersList
+                answers={props.answers}
+                onAnswerClick={props.onAnswerClick}
+            ></AnswersList>
+        </div>
+)};
 
 export default ActiveQuiz
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,15 +5,34 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 class Quiz extends React.Component {
     state = {
         quiz:[
-
+            {
+                question:'Какого цвета небо?',
+                rightAnswerId:2,
+                answers:[
+                    {text:'Черный', id:1},
+                    {text:'Синий', id:2},
+                    {text:'Красный', id:3},
+                    {text:'Зеленый', id:4}
+                ]
+            }
         ]
     }
+
+    onAnswerClickHandler = (answerId) =>{
+        console.log(answerId === this.state.quiz[0].rightAnswerId ? 'Верно' : 'Неверно');
+    }
+
     render(){
         return(
             <div className="Quiz">
                 <div className="Quiz-Wrapper">
-                    <h1>Quiz</h1>
-                    <ActiveQuiz></ActiveQuiz>
+                    <h1>Ответьте на вопросы</h1>
+                    <ActiveQuiz 
+                        question={this.state.quiz[0].question}
+                        answers={this.state.quiz[0].answers}
+                        onAnswerClick={this.onAnswerClickHandler}
+                    >                        
+                    </ActiveQuiz>
                 </div>
                 
             </div>
