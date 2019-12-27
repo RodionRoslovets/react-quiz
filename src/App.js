fd71e9762b6b4de821d76b54881f55df00059011
Rodion Roslovets <Rodion18570@gmail.com>
2019-12-27 20:05:24 +0300

    Continue creating Quiz. Practising Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Layout from './hoc/layout/Layout';
 import Quiz from './containers/Quiz/Quiz';
+import {Route, Switch} from 'react-router-dom';
+import QuizList from './containers/QuizList/QuizList';
+import Auth from './containers/Auth/Auth';
+import QuizCreator from './containers/QuizCreator/QuizCreator';
 
 class App extends React.Component {
   render(){
     return (
       <Layout>
-        <Quiz></Quiz>
+        <Switch>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/quiz-creator" component={QuizCreator}/>
+          <Route path="/quiz/:id" component={Quiz}/>
+          <Route path="/" component={QuizList}/>
+        </Switch>
       </Layout>
     );
   }
